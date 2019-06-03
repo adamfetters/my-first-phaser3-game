2dbb27a451eb8f71b90aea382a6b38cdb4d511ca
Adam Fetters <adamfetterscoder@gmail.com>
2019-06-03 10:49:35 -0400

    Added gravity and arcade physics / Setup scenes folder

---
+++ src/engineConfig.js
@@ -0,0 +1,12 @@
+export default {
+	type: Phaser.AUTO,
+	parent: 'phaser-example',
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 },
+		},
+	},
+};
